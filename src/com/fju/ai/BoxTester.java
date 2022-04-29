0ac5bf02331a6f2cccf485afacc671a447e8041d
Hank Tom <hanktom@gmail.com>
2022-04-29 10:52:45 +0800

    problem solved

--- src/com/fju/ai/BoxTester.java
+++ src/com/fju/ai/BoxTester.java
@@ -32,13 +32,16 @@ public class BoxTester {
         boxes.add(new Box1());
         boxes.add(new Box2());
         boxes.add(new Box3());
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter object's length: ");
         int length = Integer.parseInt(scanner.next());
         System.out.println("Please enter object's width: ");
         int width = Integer.parseInt(scanner.next());
         System.out.println("Please enter object's height: ");
-        int height = Integer.parseInt(scanner.next());
+        int height = Integer.parseInt(scanner.next());*/
+        int length = 12;
+        int width = 8;
+        int height = 8;
         for (Box box : boxes) {
             if (box.isFit(length, width, height)) {
                 System.out.println(box);
